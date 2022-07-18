0b6bd89c61116083bbc233e08e040ca9a4fa4609
facujalil <109365070+facujalil@users.noreply.github.com>
2022-07-17 21:41:55 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const currentLocation = location.href;
 const menuItem = document.querySelectorAll('a');
 const menuLength = menuItem.length
 
-for(let i = 0; i<menuLength; i++){
+for(let i = 1; i<menuLength; i++){
     if (menuItem[i].href === currentLocation){
 menuItem[i].className = "active"
     }
